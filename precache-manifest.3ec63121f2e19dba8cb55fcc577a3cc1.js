8bb591b958beedc9600af12ae2f8f473941a505f
Weihao Ke <weihaoke13@gmail.com>
2019-10-05 23:55:39 -0700

    Updates

---
+++ precache-manifest.3ec63121f2e19dba8cb55fcc577a3cc1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5675b27902e8ac469f26b5bbfdbd94a6",
+    "url": "/currency/index.html"
+  },
+  {
+    "revision": "8efbae33c4f4d9919bca",
+    "url": "/currency/static/css/main.2f917fa2.chunk.css"
+  },
+  {
+    "revision": "35946ca7e1cb55389f6f",
+    "url": "/currency/static/js/2.c9d72c55.chunk.js"
+  },
+  {
+    "revision": "8efbae33c4f4d9919bca",
+    "url": "/currency/static/js/main.497d3e13.chunk.js"
+  },
+  {
+    "revision": "4f0f9bc9ecc046cdbb2e",
+    "url": "/currency/static/js/runtime~main.5a41b558.js"
+  }
+]);
